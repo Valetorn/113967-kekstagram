6e0e6ccc94cb9935b560e2551f436b0459f80686
Durin Vladislav <rock_n_vladok@mail.ru>
2017-02-08 22:18:52 +0400

    Fixed filters choise

--- js/form.js
+++ js/form.js
@@ -6,11 +6,9 @@ var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
 var filterImagePreview = document.querySelector('.filter-image-preview');
 var controls = document.querySelectorAll('.upload-filter-controls input[type = "radio"]');
 var uploadFilterControls = document.querySelector('.upload-filter-controls');
-var filterDefault = document.getElementById('upload-filter-none');
 var uploadResizeControlsButtonDec = uploadOverlay.querySelector('.upload-resize-controls-button-dec');
 var uploadResizeControlsButtonInc = uploadOverlay.querySelector('.upload-resize-controls-button-inc');
 var uploadResizeControlsValue = uploadOverlay.querySelector('.upload-resize-controls-value');
-
 
 var ENTER_KEY_CODE = 13;
 var ESCAPE_KEY_CODE = 27;
@@ -18,23 +16,27 @@ var ESCAPE_KEY_CODE = 27;
 function activateElem(evt) {
   return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
 }
+function changeAriaAttribute(isOpen) {
+  uploadOverlay.setAttribute('aria-pressed', !isOpen);
+}
 function uploadOverlayKeydownHandler(evt) {
   if (evt.keyCode === ESCAPE_KEY_CODE) {
     closeUploadOverlay();
+    changeAriaAttribute(false);
   }
 }
 function openUploadOverlay() {
   uploadSelectImage.classList.add('invisible');
   uploadOverlay.classList.remove('invisible');
   document.addEventListener('keydown', uploadOverlayKeydownHandler);
-  uploadOverlay.setAttribute('aria-hidden', 'false');
+  changeAriaAttribute(true);
 }
 function closeUploadOverlay() {
   uploadOverlay.classList.add('invisible');
   uploadSelectImage.classList.remove('invisible');
   document.removeEventListener('keydown', uploadOverlayKeydownHandler);
-  uploadOverlay.setAttribute('aria-hidden', 'true');
-} 
+  changeAriaAttribute(false);
+}
 
 uploadFile.addEventListener('change', function () {
   openUploadOverlay();
@@ -68,14 +70,16 @@ function setFilter(evt) {
   for (var i = 0; i < controls.length; i++) {
     filterImagePreview.classList.remove('filter-' + controls[i].value);
   }
-  
   var filter = evt.target;
-  
   if (filter.tagName === 'INPUT') {
     filterImagePreview.classList.add('filter-' + filter.value);
   }
+  if (filter.tagName === 'LABEL') {
+    var inputFilter = document.getElementById(filter.htmlFor);
+    inputFilter.checked = true;
+    filterImagePreview.classList.add('filter-' + inputFilter.value);
+  }
 }
-
 uploadFilterControls.addEventListener('click', function (evt) {
   setFilter(evt);
 });
@@ -85,23 +89,4 @@ uploadFilterControls.addEventListener('keydown', function (evt) {
   }
 });
 
-/*for (var i = 0; i < controls.length; i++) {
-  clickControl(controls[i]);
-}
-
-function getFilter(control) {
-  for (i = 0; i < controls.length; i++) {
-    filterImagePreview.classList.remove('filter-' + controls[i].value);
-  }
-  if (filterImagePreview) {
-    filterImagePreview.classList.add('filter-' + control.value);
-  }
-}
-function clickControl(control) {
-  control.addEventListener('click', function () {
-    getFilter(control);
-  });
-}
-
-getFilter(filterDefault);*/
 
