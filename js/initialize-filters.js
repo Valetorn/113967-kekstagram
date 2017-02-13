42f60a1b6312032c6e3836d9cda53ddec798f9ee
Durin Vladislav <rock_n_vladok@mail.ru>
2017-02-13 21:20:16 +0400

    Incapsulated modules

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,16 +1,18 @@
 'use strict';
 
-window.initializeFilters = function (evt) {
-  for (var i = 0; i < window.controls.length; i++) {
-    window.filterImagePreview.classList.remove('filter-' + window.controls[i].value);
-  }
-  var filter = evt.target;
-  if (filter.tagName === 'INPUT') {
-    window.filterImagePreview.classList.add('filter-' + filter.value);
-  }
-  if (filter.tagName === 'LABEL') {
-    var inputFilter = document.getElementById(filter.htmlFor);
-    inputFilter.checked = true;
-    window.filterImagePreview.classList.add('filter-' + inputFilter.value);
-  }
-};
+window.initializeFilters = (function () {
+  return function (evt) {
+    for (var i = 0; i < window.controls.length; i++) {
+      window.filterImagePreview.classList.remove('filter-' + window.controls[i].value);
+    }
+    var filter = evt.target;
+    if (filter.tagName === 'INPUT') {
+      window.filterImagePreview.classList.add('filter-' + filter.value);
+    }
+    if (filter.tagName === 'LABEL') {
+      var inputFilter = document.getElementById(filter.htmlFor);
+      inputFilter.checked = true;
+      window.filterImagePreview.classList.add('filter-' + inputFilter.value);
+    }
+  };
+})();
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,22 +1,26 @@
 'use strict';
 
-function maxOrMinValue() {
-  if (parseInt(window.uploadResizeControlsValue.value, 10) < 25) {
-    window.uploadResizeControlsValue.value = 25 + '%';
-  }
-  if (parseInt(window.uploadResizeControlsValue.value, 10) > 100) {
-    window.uploadResizeControlsValue.value = 100 + '%';
-  }
-  return window.uploadResizeControlsValue.value;
-}
-window.createScale = function (evt) {
-  var resizeStep = 25;
-  var resizeControl = evt.target;
-  if (resizeControl.className === window.uploadResizeControlsButtonDec.className) {
-    window.uploadResizeControlsValue.value = window.parseInt(window.uploadResizeControlsValue.value, 10) - resizeStep + '%';
-  } else if (resizeControl.className === window.uploadResizeControlsButtonInc.className) {
-    window.uploadResizeControlsValue.value = window.parseInt(window.uploadResizeControlsValue.value, 10) + resizeStep + '%';
-  }
-  var scaleNumber = parseInt(maxOrMinValue(), 10) / 100;
-  window.filterImagePreview.style.transform = 'scale(' + scaleNumber + ')';
-};
+var maxOrMinValue = (function () {
+  return function () {
+    if (parseInt(window.uploadResizeControlsValue.value, 10) < 25) {
+      window.uploadResizeControlsValue.value = 25 + '%';
+    }
+    if (parseInt(window.uploadResizeControlsValue.value, 10) > 100) {
+      window.uploadResizeControlsValue.value = 100 + '%';
+    }
+    return window.uploadResizeControlsValue.value;
+  };
+})();
+window.createScale = (function () {
+  return function (evt) {
+    var resizeStep = 25;
+    var resizeControl = evt.target;
+    if (resizeControl.className === window.uploadResizeControlsButtonDec.className) {
+      window.uploadResizeControlsValue.value = window.parseInt(window.uploadResizeControlsValue.value, 10) - resizeStep + '%';
+    } else if (resizeControl.className === window.uploadResizeControlsButtonInc.className) {
+      window.uploadResizeControlsValue.value = window.parseInt(window.uploadResizeControlsValue.value, 10) + resizeStep + '%';
+    }
+    var scaleNumber = parseInt(maxOrMinValue(), 10) / 100;
+    window.filterImagePreview.style.transform = 'scale(' + scaleNumber + ')';
+  };
+})();
